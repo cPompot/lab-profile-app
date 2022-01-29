97efdbe465d08bb498996ba9cc573998341c6aaa
hassan sabeh <h.adamsabeh@yahoo.com>
2022-01-29 11:40:21 +0100

    server WIP

--- lab-profile-app/server/app.js
+++ lab-profile-app/server/app.js
@@ -20,7 +20,7 @@ const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
 const auth = require("./routes/auth");
-app.use("/api/auth", auth);
+app.use("/api", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
