0466e3d27650d1292ad43ed831b9c962e2235f5e
Claire Pottier <claire_pottier@hotmail.fr>
2022-01-29 11:28:47 +0100

    iteration 1

---
+++ lab-profile-app/server/routes/auth.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const User = require('../../models/User.model');
+const bcryptjs = require('bcryptjs');
+
+const saltRounds = 10;
+const salt = bcryptjs.genSaltSync(saltRounds);
+
+router.get('/auth/signup', (req, res, next) => {
+    res.status(200).json({message: 'Signup page'})
+})
+
+router.post('/signup', (req, res, next) => {
+    const {username, password} = req.body
+
+    if (!username || !password) {
+        res.status(400).json({message: 'Provide username and password'});
+        return;
+    }
+
+    User.findOne({ username })
+        .then(foundUser => {
+            if (foundUser) {
+                res.status(403).json({message: 'User already exists'});
+            } else {
+                const hashPassword = bcryptjs.hashSync(password, salt);
+                User.create(username, hashPassword)
+                    .then(userFromDb => {
+                        res.status(201).json({message: 'User correctly created'})
+                    })
+                    .catch(error => {
+                        res.status(400).json({message: `Saving user to database went wrong, error: ${error}`})
+                    })
+            }
+        })  
+        .catch(error => {
+            res.status(400).json({message: `Error validating user, error: ${error}`})
+        })
+})
+
+module.exports = router;
